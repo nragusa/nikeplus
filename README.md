9215907b64ffc14443068309c977f85feb9944e3
Nick Ragusa <nragusa@gmail.com>
2017-03-13 11:17:20 -0400

    Initial add

---
+++ README.md
@@ -0,0 +1,66 @@
+# Nike+ API and AWS Athena
+
+This is a simple script which uses the Nike+ API to retrieve RUNNING activities, update those activities with GPS coordinates (if applicable), writes the information in a multi-line, JSON formatted file, then uploads the file to an S3 bucket of your choice. 
+
+## Getting Started
+
+Before you run the script, ensure you have the following:
+* An API access token from Nike+. Technically one should use API keys, but as far as I can tell, these are only open to
+[partners](https://developer.nike.com/contact-us.html). To work around this, you can simply [log in](https://developer.nike.com/content/nike-developer-cq/us/en_us/index/login.html), then use something like Chrome developer tools to go to Application -> Storage -> Local Storage -> https://api.nike.com/, and look for the *_access_token key and corresponding value. That will be your short lived access_token to use with this script.
+* [Create an S3 bucket](http://docs.aws.amazon.com/AmazonS3/latest/gsg/CreatingABucket.html).
+* Ensure you have an appropriate AWS [access_key_id and secret_access_key](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html). Also make sure they are [stored properly](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html).
+
+### Prerequisites
+
+Create a python [virtualenv](http://docs.python-guide.org/en/latest/dev/virtualenvs/), and use the requirements.txt file to install all the necessary components:
+
+```
+pip install -r requirements.txt
+```
+
+### Example Usage
+
+To execute the script, you'll need to pass the appropriate token, number of previous runs to capture, S3 bucket, and filename to store the data in. 
+
+The following will grab the latest 3 runs from Nike+, gather GPS data for each of the runs, write the run information to a local file, then upload it to the S3 bucket *myS3Bucket* with a key of *running.json*.
+
+```
+./update.py --token iou23jk230sdfkj0a032kljasdf0 --runs 3 --bucket myS3Bucket --key running.json
+```
+
+## Optional: Query with Amazon Athena
+
+[Amazon Athena](https://aws.amazon.com/athena/) is an interactive, ad-hoc query service which makes it easy to query data stored in S3 using traditional ANSI SQL like syntax. With this data now stored in a multi-line, JSON formatted syntax, you can now query it using Amazon Athena. See the [Getting Started](https://docs.aws.amazon.com/athena/latest/ug/getting-started.html) to get going with Amazon Athena.
+
+Average number of miles by terrain:
+```
+SELECT count(tag.tagValue) AS count,
+         avg(cast(metricSummary.distance AS double)) AS average,
+         tag.tagValue
+FROM activity.running
+CROSS JOIN unnest(tags) AS t(tag)
+WHERE tag.tagType = 'TERRAIN'
+GROUP BY  tag.tagValue
+```
+
+Total miles in 2014:
+```
+SELECT sum(cast(metricSummary.distance AS double))
+FROM activity.running
+WHERE starttime > varchar '2014-01-01' and starttime < varchar '2014-12-31';
+```
+
+Total miles by shoe:
+```
+SELECT count(tag.tagValue) AS runs,
+         sum(cast(metricSummary.distance AS double)) AS miles,
+         tag.tagValue AS shoes
+FROM activity.running
+CROSS JOIN unnest(tags) AS t(tag)
+WHERE tag.tagType = 'SHOES'
+GROUP BY  tag.tagValue
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ update.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+"""This program takes in a Nike access token and a total number of runs
+to retrieve from Nike, starting with the most recent run first. It will
+then call the Nike API a second time to retrieve GPS coordinates for
+each run, write this run data to a file, then upload the file to an S3
+bucket. This resulting file is formatted in such a way that it can be
+queried by Amazon Athena.
+"""
+__version__ = '0.1'
+__author__ = 'Nick Ragusa'
+
+import argparse
+import json
+import sys
+
+import boto3
+import requests
+from pygeocoder import Geocoder
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-t", "--token", help="nike access token", type=str, required=True)
+PARSER.add_argument("-r", "--runs", help="number of runs back", type=int, required=True)
+PARSER.add_argument("-b", "--bucket", help="S3 bucket", type=str, required=True)
+PARSER.add_argument("-k", "--key", help="The S3 key for the file", type=str, required=True)
+ARGS = PARSER.parse_args()
+KM_TO_MILES = 0.621371
+
+def get_gps(aid, token):
+    """Returns lattitude, longitude, and elevation coordinates
+    of the beginning and end of a run.
+
+    Keyword arguments:
+    aid -- unique activity ID of the run
+    token -- nike API access token
+    """
+    url = 'https://api.nike.com/v1/me/sport/activities/{}/gps?access_token={}'.format(aid, token)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.HTTPError as error:
+        print 'Problem calling Nike API: {}'.format(error)
+        sys.exit(1)
+    else:
+        try:
+            data = json.loads(response.text)
+        except ValueError:
+            print 'Problem loading Nike response into JSON'
+            sys.exit(1)
+        if response.status_code != 200:
+            return ({'latitude': 0, 'longitude': 0, 'elevation': 0},
+                    {'latitude': 0, 'longitude': 0, 'elevation': 0})
+        else:
+            return data['waypoints'][0], data['waypoints'][-1]
+
+def main():
+    """Runs the main program"""
+    access_token = ARGS.token
+    runs = ARGS.runs
+    s3_bucket = ARGS.bucket
+    s3_key = ARGS.key
+    url = ('https://api.nike.com/v1/me/sport/activities/RUNNING?count={}'
+           '&access_token={}'.format(runs, access_token))
+    try:
+        response = requests.get(url)
+    except requests.exceptions.HTTPError as error:
+        print 'Problem calling Nike API: {}'.format(error)
+        sys.exit(1)
+    else:
+        if response.status_code != 200:
+            print 'Non-200 code returned from Nike API'
+            sys.exit(1)
+        try:
+            run_data = json.loads(response.text)
+        except ValueError:
+            print 'Problem loading Nike response into JSON'
+            sys.exit(1)
+
+    with open('running-etl.json', 'a') as running_etl_file:
+        for run in run_data['data']:
+            gps_start, gps_end = get_gps(run['activityId'], access_token)
+            if gps_start['latitude'] != 0:
+                geo = Geocoder.reverse_geocode(gps_start['latitude'], gps_start['longitude'])
+                run['postal'] = geo.postal_code
+                run['city'] = geo.city
+                run['state'] = geo.state
+            else:
+                run['postal'] = 'Unknown'
+                run['city'] = 'Unknown'
+                run['state'] = 'Unknown'
+            run['gpsStart'] = gps_start
+            run['gpsEnd'] = gps_end
+            run['metricSummary']['distance'] = str(float(run['metricSummary']['distance'])
+                                                   * KM_TO_MILES)
+            json.dump(run, running_etl_file)
+            running_etl_file.write('\n')
+
+    with open('running-etl.json', 'r') as running_file:
+        s3_client = boto3.resource('s3')
+        s3_client.Bucket(s3_bucket).put_object(Key=s3_key, Body=running_file)
+
+if __name__ == '__main__':
+    main()
